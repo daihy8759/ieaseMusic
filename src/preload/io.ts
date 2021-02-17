27b3e511315f7175c37c40c4be57540ed6378c45
daihy <dai875939260@163.com>
2021-02-17 18:53:52 +0800

    :poop: Remove unsafe method

--- src/preload/io.ts
+++ src/preload/io.ts
@@ -1,17 +1,4 @@
 import path from 'path';
-import fs from 'fs';
-
-function removeFile(path: string): Promise<void> {
-    return new Promise((resolve) => {
-        if (fs.existsSync(path)) {
-            fs.unlink(path, () => {
-                resolve();
-            });
-        } else {
-            resolve();
-        }
-    });
-}
 
 export default {
     basename(p: string, ext?: string) {
@@ -19,19 +6,5 @@ export default {
             return path.basename(p, ext);
         }
         return path.basename(p);
-    },
-    async removeFiles(filenames: string[]) {
-        await Promise.all(filenames.map((filename) => removeFile(filename)));
-    },
-    copyFile(source: string, dest: string) {
-        return new Promise<void>((resolve, reject) => {
-            fs.copyFile(source, dest, (err: any) => {
-                if (err) {
-                    reject();
-                } else {
-                    resolve();
-                }
-            });
-        });
     },
 };
--- src/renderer/hooks/io.ts
+++ src/renderer/hooks/io.ts
@@ -1,7 +1,5 @@
 interface IO {
     basename: (filename: string, ext?: string) => string;
-    removeFiles: (filenames: string[]) => Promise<void>;
-    copyFile: (source: string, dest: string) => Promise<void>;
 }
 
 const io = (window as any).io as IO;
